894755e034059bb36a992d1a92393a2f142d2970
Kristina <yarevyakinakristina@gmail.com>
2023-02-14 13:22:05 +0700

    created header

---
+++ components/header/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <TheLogo/>
+        <TheHeaderNavigation/>
+    </header>
+</template>
+
+<script>
+import TheLogo from './TheLogo.vue';
+import TheHeaderNavigation from './TheHeaderNavigation.vue';
+
+export default {
+    name: "TheHeader",
+    components: { TheLogo, TheHeaderNavigation}
+}
+</script>
+
+<style scoped>
+ header {
+    display: flex;
+ }
+</style>
---
+++ components/header/TheHeaderNavigation.vue
@@ -0,0 +1,60 @@
+<template>
+    <nav class="header-navigation__container">      
+        <ul>        
+            <li><NuxtLink to="/" class="header-navigation__link">
+                <img src="./../../assets/icons/Union.svg" alt="" loading="lazy">
+                <p>Главная</p>
+            </NuxtLink></li>        
+            <li><NuxtLink to="#" class="header-navigation__link">
+                <img src="./../../assets/icons/help.svg" alt="" loading="lazy">
+                <p>Помощь</p>
+            </NuxtLink></li>        
+            <li><NuxtLink to="#" class="header-navigation__link">
+                <img src="./../../assets/icons/base.svg" alt="" loading="lazy">
+                <p>База знаний</p>
+            </NuxtLink></li>      
+        </ul>   
+    </nav> 
+</template>
+
+<script>
+export default {
+  name: "TheHeaderNavigation"
+}
+</script>
+
+<style scoped>
+
+ * {
+    padding: 0;
+    margin: 0;
+ }
+ .header-navigation__container ul {
+    display: flex;
+    margin-right: 18px;
+    text-decoration: none;
+ }
+
+ .header-navigation__container ul li {
+    margin-right: 18px;
+    list-style-type: none;
+ }
+
+ .header-navigation__link {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #000000;
+    font-family: 'Open Sans', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 22px;
+ }
+
+ .header-navigation__link img {
+    margin-right: 7px;
+ }
+
+
+</style>
